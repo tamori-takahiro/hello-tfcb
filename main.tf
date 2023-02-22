721766d6b96a483e15f1e927cb735d7dfc931df7
tamori-takahiro <tamori-takahiro@hd.7andi.co.jp>
2023-02-22 07:39:50 +0000

    fix apach enabled 2

--- main.tf
+++ main.tf
@@ -100,7 +100,7 @@ resource "null_resource" "configure-cat-app" {
     ]
     connection {
       type        = "ssh"
-      user        = "ubunth"
+      user        = "ubuntu"
       private_key = tls_private_key.hashicat.private_key_pem
       host        = aws_eip.hashicat.public_ip
     }
