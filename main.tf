c5999edeb006f3234dbf4d283ea97a7476495e7c
tamori-takahiro <tamori-takahiro@hd.7andi.co.jp>
2023-02-22 06:53:23 +0000

    ssh enabled

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "tamori-takahiro"
+    hostname     = "app.terraform.io"
+    organization = ORGNAME
     workspaces {
       name = "hello-tf"
     }
@@ -11,22 +11,69 @@ terraform {
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
-  region = var.region
+  region     = var.region
 }
 
 resource "aws_vpc" "hashicat" {
-  cidr_block = var.address_space
+  cidr_block           = var.address_space
   enable_dns_hostnames = true
 }
 
 resource "aws_subnet" "hashicat" {
-  vpc_id = aws_vpc.hashicat.id
+  vpc_id     = aws_vpc.hashicat.id
   cidr_block = var.subnet_prefix
 }
 
 resource "aws_instance" "hashicat" {
-  ami = var.ami
-  count = var.hello_tf_instance_count
-  instance_type = var.hello_tf_instance_type
-  subnet_id = aws_subnet.hashicat.id
+  ami                         = var.ami
+  count                       = var.hello_tf_instance_count
+  instance_type               = var.hello_tf_instance_type
+  subnet_id                   = aws_subnet.hashicat.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.hashicat.id]
+}
+
+resource "aws_security_group" "hashicat" {
+  name   = "hashicat-security-group"
+  vpc_id = aws_vpc.hashicat.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+}
+
+resource "aws_internet_gateway" "hashicat" {
+  vpc_id = aws_vpc.hashicat.id
+}
+
+resource "aws_route_table" "hashicat" {
+  vpc_id = aws_vpc.hashicat.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.hashicat.id
+  }
+}
+
+resource "aws_route_table_association" "hashicat" {
+  subnet_id      = aws_subnet.hashicat.id
+  route_table_id = aws_route_table.hashicat.id
+}
+
+resource "aws_eip" "hashicat" {
+  instancce = aws_instance.hashicat.id
+  vpc       = true
+}
+
+resource "aws_eip_association" "hashicat" {
+  instance_id   = aws_instance.hashicat.id
+  allocation_id = aws_eip.hashicat.id
 }
